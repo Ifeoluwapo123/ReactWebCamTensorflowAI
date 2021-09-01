a9011b0b5775480070f12dbf69b79696571e26ee
Ifeoluwapo123 <adenusidamilola5@gmail.com>
2021-09-01 21:25:18 +0100

    react face detection

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
-// 1. Install dependencies DONE
-// 2. Import dependencies DONE
-// 3. Setup webcam and canvas DONE
-// 4. Define references to those DONE
-// 5. Load posenet DONE
-// 6. Detect function DONE
-// 7. Drawing utilities from tensorflow DONE
-// 8. Draw functions DONE
-
-// Face Mesh - https://github.com/tensorflow/tfjs-models/tree/master/facemesh
-
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import "./App.css";
 import * as tf from "@tensorflow/tfjs";
-// OLD MODEL
-//import * as facemesh from "@tensorflow-models/facemesh";
-
-// NEW MODEL
 import * as facemesh from "@tensorflow-models/face-landmarks-detection";
 import Webcam from "react-webcam";
 import { drawMesh } from "./utilities";
@@ -23,16 +8,17 @@ import { drawMesh } from "./utilities";
 function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
+  const imageRef = useRef(null);
+  const snap = useRef(null);
+  const reCapture = useRef(null);
+  const [faceDetected, setFaceDetected] = useState(false);
+  const [imageCaptured, setImgCaptured] = useState(false);
+  const [webcam, setWebcam] = useState(true);
 
-  //  Load posenet
   const runFacemesh = async () => {
-    // OLD MODEL
-    // const net = await facemesh.load({
-    //   inputResolution: { width: 640, height: 480 },
-    //   scale: 0.8,
-    // });
-    // NEW MODEL
-    const net = await facemesh.load(facemesh.SupportedPackages.mediapipeFacemesh);
+    const net = await facemesh.load(
+      facemesh.SupportedPackages.mediapipeFacemesh
+    );
     setInterval(() => {
       detect(net);
     }, 10);
@@ -58,25 +44,79 @@ function App() {
       canvasRef.current.height = videoHeight;
 
       // Make Detections
-      // OLD MODEL
-      //       const face = await net.estimateFaces(video);
-      // NEW MODEL
-      const face = await net.estimateFaces({input:video});
-      console.log(face);
+      const face = await net.estimateFaces({ input: video });
 
       // Get canvas context
       const ctx = canvasRef.current.getContext("2d");
-      requestAnimationFrame(()=>{drawMesh(face, ctx)});
+      setFaceDetected(face.length !== 0);
+      // requestAnimationFrame(()=>{drawMesh(face, ctx)});
     }
   };
 
-  useEffect(()=>{runFacemesh()}, []);
+  useEffect(() => {
+    runFacemesh();
+  }, []);
+
+  const discardImage = () => {
+    setWebcam((prev) => !prev);
+    reCapture.current.style.display = "none";
+    snap.current.style.display = "";
+  };
+
+  const onCaptureHandler = React.useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    imageRef.current.style.display = "";
+    reCapture.current.style.display = "";
+    snap.current.style.display = "none";
+    imageRef.current.setAttribute("src", imageSrc);
+    setWebcam((prev) => !prev);
+    setImgCaptured(true);
+  }, [webcamRef]);
+
+  const convertToFile = async () => {
+    const fileName = "photo" + Date.now() + ".png";
+    const res = await fetch(imageRef.current.src);
+    const blob = await res.blob();
+
+    console.log(new File([blob], fileName, { type: "image/png" }));
+  };
 
   return (
     <div className="App">
+      <button
+        disabled={!faceDetected}
+        ref={snap}
+        onClick={onCaptureHandler}
+        style={{ backgroundColor: "#fff", width: "319px", height: "60px" }}
+      >
+        Capture
+      </button>
+      <button
+        ref={reCapture}
+        style={{
+          backgroundColor: "#fff",
+          width: "319px",
+          height: "60px",
+          display: "none",
+        }}
+        onClick={discardImage}
+      >
+        Recapture
+      </button>
+      <button
+        disabled={!imageCaptured}
+        style={{
+          backgroundColor: "#fff",
+          width: "319px",
+          height: "60px",
+        }}
+        onClick={convertToFile}
+      >
+        Done
+      </button>
       <header className="App-header">
-        <Webcam
-          ref={webcamRef}
+        <img
+          ref={imageRef}
           style={{
             position: "absolute",
             marginLeft: "auto",
@@ -87,9 +127,27 @@ function App() {
             zindex: 9,
             width: 640,
             height: 480,
+            display: "none",
           }}
         />
-
+        {webcam ? (
+          <Webcam
+            ref={webcamRef}
+            style={{
+              position: "absolute",
+              marginLeft: "auto",
+              marginRight: "auto",
+              left: 0,
+              right: 0,
+              textAlign: "center",
+              zindex: 9,
+              width: 640,
+              height: 480,
+            }}
+          />
+        ) : (
+          <></>
+        )}
         <canvas
           ref={canvasRef}
           style={{
--- src/utilities.js
+++ src/utilities.js
@@ -2683,7 +2683,7 @@ export const drawMesh = (predictions, ctx) => {
 
         ctx.beginPath();
         ctx.arc(x, y, 1 /* radius */, 0, 3 * Math.PI);
-        ctx.fillStyle = "aqua";
+        ctx.fillStyle = "white";
         ctx.fill();
       }
     });
